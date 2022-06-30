729af2f20a9273e8ffd4e2a7aa37f2aa885cb3b1
Avrigeanu Sebastian <sand.avrigeanu@yahoo.com>
2022-06-30 22:42:35 +0300

    Login form created with database connection

---
+++ WPFBookstore/Login.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Npgsql;
+
+namespace WPFBookstore
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void loginbutton_Click(object sender, RoutedEventArgs e)
+        {
+            bool found = false;
+            try
+            {
+                using (var conn = new NpgsqlConnection("Host = localhost; Username = postgres; Password = password1; Database = WPFBookstore"))
+                {
+                    await conn.OpenAsync();
+                    string sql = "SELECT * from accounts where username='" + usernamebox.Text + "' and password='" + passwordbox.Text + "'";
+                    using var cmd = new NpgsqlCommand(sql, conn);
+                    using NpgsqlDataReader rdr = cmd.ExecuteReader();
+                   if(rdr.Read())
+                    {
+                        found = true;
+                        MessageBox.Show("Login Successful");
+                    }
+                    if (found == false)
+                    {
+                        MessageBox.Show("Invalid username or password");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
